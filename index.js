634692e29d5089fa760bb204ade999a61170fe2a
Tatyana Kritskaya <kritskaya.tatjana@gmail.com>
2022-12-11 17:47:56 +0300

    feat: show current dir after each user input

--- index.js
+++ index.js
@@ -5,20 +5,24 @@ import * as readline from 'readline/promises';
 const userArg = argv.find((arg) => arg.startsWith('--username='));
 const username = userArg ? userArg.slice(11) : 'Noname';
 
+let currentPath = os.homedir();
+
 stdout.write(`Welcome to the File Manager, ${username}!${os.EOL}`);
+stdout.write(`You are currently in ${currentPath}`);
 
 const rl = readline.createInterface({ input: stdin, output: stdout });
 
 rl.on('line', (input) => {
+  
   if (input === '.exit') {
     rl.close();
   }
-})
+
+  stdout.write(`You are currently in ${currentPath}${os.EOL}`);
+});
 
 rl.on('close', () => {
   stdout.write(
     `Thank you for using File Manager, ${username}, goodbye!${os.EOL}`
   );
 });
-
-
