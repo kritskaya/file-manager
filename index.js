ee1ed80990523cc336ee5c12a948b15a2bae1990
Tatyana Kritskaya <kritskaya.tatjana@gmail.com>
2022-12-11 17:30:43 +0300

    feat: implement exit program feature

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 import { stdin, stdout, argv } from 'process';
 import os from 'os';
+import * as readline from 'readline/promises';
 
-const username =
-  argv.find((arg) => arg.startsWith('--username=')).slice(11) || 'Noname';
-  
+const userArg = argv.find((arg) => arg.startsWith('--username='));
+const username = userArg ? userArg.slice(11) : 'Noname';
+
 stdout.write(`Welcome to the File Manager, ${username}!${os.EOL}`);
+
+const rl = readline.createInterface({ input: stdin, output: stdout });
+
+rl.on('line', (input) => {
+  if (input === '.exit') {
+    rl.close();
+  }
+})
+
+rl.on('close', () => {
+  stdout.write(
+    `Thank you for using File Manager, ${username}, goodbye!${os.EOL}`
+  );
+});
+
+
