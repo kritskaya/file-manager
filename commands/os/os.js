beb352d1e8384de84dbee40d85b1567d7a88f633
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2022-12-17 10:40:59 +0300

    feat: implement os command

--- commands/os/os.js
+++ commands/os/os.js
@@ -8,17 +8,28 @@ export const os = (args) => {
     case '--homedir':
       stdout.write(`home directory: ${cwd()}${OS.EOL}`);
       break;
+    case '--cpus':
+      const cpus = OS.cpus();
+      stdout.write(`overall amount of CPUS: ${cpus.length}${OS.EOL}`);
+      stdout.write(`information about each logical CPU core:${OS.EOL}`);
+      cpus.forEach((cpu) => {
+        stdout.write(`model: ${cpu.model}${OS.EOL}`);
+        stdout.write(`clock rate: ${Math.round(cpu.speed / 100) / 10}GHz${OS.EOL}`);
+      });
+      break;
     case '--username':
-      stdout.write(`current system user name: ${OS.userInfo().username}`);
+      stdout.write(
+        `current system user name: ${OS.userInfo().username}${OS.EOL}`
+      );
       break;
     case '--architecture':
-      stdout.write(`CPU architecture: ${OS.arch()}`);
+      stdout.write(`CPU architecture: ${OS.arch()}${OS.EOL}`);
       break;
     case '--EOL':
       stdout.write(
         `default system End-Of-Line: ${
           OS.platform() === 'win32' ? '\\r\\n' : '\\n'
-        }`
+        }${OS.EOL}`
       );
       break;
     default:
